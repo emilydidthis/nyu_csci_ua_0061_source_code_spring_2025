56cbc59deaabbbbedf107ef1482be132f0c997b4
Emily Zhao <35831739+emilydidthis@users.noreply.github.com>
2025-01-28 14:18:54 -0500

    javascript class notes

---
+++ days_03_04/intro_to_javascript/02_JavaScriptOutput.js
@@ -0,0 +1,15 @@
+// document.write writes a string of text into your HTML document
+document.write("<p>document.write: hello, world!</p>");
+
+// alert pops up an alert box (try not to use this much!)
+alert("alert: hello, world!");
+
+// console.log writes a string of text to the JavaScript console (View -> Developer -> JavaScript Console in Chrome)
+console.log("console.log: hello, world!");
+
+/* 
+const a = "hey";
+const b = "there";
+const c = a + b;
+(a+b).toUpperCase();
+*/
