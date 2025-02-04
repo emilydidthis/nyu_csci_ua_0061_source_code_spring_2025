5e3b8f9ec5c1bb20f9b84424e49a1bd93c941f2d
Emily Zhao <35831739+emilydidthis@users.noreply.github.com>
2025-02-04 15:05:21 -0500

    days 5 and 6

---
+++ days_05_06/DOM_queries_attributes_styles_simple_events/03_MouseEventDemo.js
@@ -0,0 +1,8 @@
+// grab a reference to the element with id 'box'
+
+
+// grab a reference to the 'history' panel
+
+
+// attach a series of mouse events to this element with id 'box'
+// each of these events will update the 'history' panel
---
+++ days_05_06/DOM_queries_attributes_styles_simple_events/04_MakingThingsVisibleAndInvisible.js
@@ -0,0 +1,8 @@
+// grab a reference to our images
+
+
+// listen for click events on Pikachu button
+
+
+// listen for click events on Charmander button
+
