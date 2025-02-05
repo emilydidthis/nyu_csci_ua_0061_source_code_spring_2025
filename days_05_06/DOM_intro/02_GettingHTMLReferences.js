d99d6e81c7ee7fb5a2f6c67d27931ca300d46b42
Emily Zhao <35831739+emilydidthis@users.noreply.github.com>
2025-02-04 19:29:07 -0500

    code solutions for day 5

--- days_05_06/DOM_intro/02_GettingHTMLReferences.js
+++ days_05_06/DOM_intro/02_GettingHTMLReferences.js
@@ -25,11 +25,11 @@ console.log(element1Styles.getPropertyValue("color")); //use getPropertyValue to
 
 // you can adjust the CSS styles on this element by using the
 // 'style' property
-// element1.style.color = 'green';
-// element1.style['font-family'] = 'Fantasy';
-// element1.style['font-size'] = '200%';
-// element1.style['text-decoration'] = 'none';
+element1.style.color = 'green';
+element1.style['font-family'] = 'Fantasy';
+element1.style['font-size'] = '200%';
+element1.style['text-decoration'] = 'none';
 
 // one method to adjust the text inside of an element is to use the
 // 'innerText' property on the element, like this:
-// element1.innerText = 'Click here to go to NYU';
+element1.innerText = 'Click here to go to NYU';
