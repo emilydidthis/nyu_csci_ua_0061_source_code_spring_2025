ffb7eafad1907b539e148d1ab9754b36588c3046
Emily Zhao <35831739+emilydidthis@users.noreply.github.com>
2025-02-11 15:16:52 -0500

    day 7 and 8

---
+++ days_07_08/01-DOM_classes_selecting_multiple_elements_and_creating_new_elements/04_ProgrammingChallenge.js
@@ -0,0 +1,17 @@
+// grab references to each of our tabs
+const tabWelcome = document.getElementById('tabWelcome');
+const tabTraditional = document.getElementById('tabTraditional');
+const tabGlazed = document.getElementById('tabGlazed');
+
+// grab references to each content panel
+const contentWelcome = document.getElementById('contentWelcome');
+const contentTraditional = document.getElementById('contentTraditional');
+const contentGlazed = document.getElementById('contentGlazed');
+
+
+//  1. event handler to make our Welcome Tab active
+  // first, make this tab our 'active' tab by applying the 'active' class to it
+  // remove the 'active' class from the other tabs
+  // show the content associated with this tab
+  // hide the other two contents
+
---
+++ days_07_08/02-DOM_augmentation_pointers/08_Example-SelectOnlyOnePokemon.js
@@ -0,0 +1,9 @@
+// set up every image on the page to respond to click events
+// (get a reference to all of the images using their class name)
+
+    // if the image is clicked, add the blue class to it
+
+
+    // what happens when you click a second image?
+    // we only want one selected image at a time
+
