a30497cf1eec4d88aaf63dffbcc8ec71c896ec01
Emily Zhao <35831739+emilydidthis@users.noreply.github.com>
2025-02-20 14:59:31 -0500

    uploading days 9 and 10

---
+++ days_09_10/06_ImageGallery.js
@@ -0,0 +1,7 @@
+// When an Image is Clicked:
+
+// 1. Update the caption: Retrieve a description from the image’s data and display it.
+// 2. Show a larger version: Replace the current large image with a bigger version of the clicked image.
+// 3. Track how many times the image has been clicked:
+//          If it’s the first time, initialize a counter.
+//          Otherwise, increase the count and display the updated number.
---
+++ days_09_10/07_TabbingSystemWithDataset.js
@@ -0,0 +1,5 @@
+// When a Tab is Clicked:
+//     Deactivate the currently active tab.
+//     Activate the clicked tab.
+//     Hide all content panels (so only one is visible).
+//     Show the correct content panel associated with the clicked tab.
